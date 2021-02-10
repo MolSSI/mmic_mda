acc5c068e00628cad2ee967e366cf8b74e504ea2
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-02-10 16:16:32 -0500

    added gro file to test funcs

--- mmic_mda/tests/test_mmic_mda_mol.py
+++ mmic_mda/tests/test_mmic_mda_mol.py
@@ -12,7 +12,10 @@ import mmelemental as mm
 
 
 data_dir = os.path.join("mmic_mda", "data")
-top_file = os.path.join(data_dir, "molecules", "1dzl_fixed.pdb")
+top_file = [
+    os.path.join(data_dir, "molecules", file)
+    for file in ("1dzl_fixed.pdb", "1dzl_fixed.gro")
+]
 
 
 def pytest_generate_tests(metafunc):
@@ -26,22 +29,22 @@ def test_mmic_mda_imported():
 
 
 def test_mda_to_mol(guess_bonds):
-    uni = mda.Universe(top_file, guess_bonds=guess_bonds)
-    mda_mol = mmic_mda.models.MdaMol(data=uni)
-    mm_mol = mmic_mda.components.MdaToMolComponent.compute(mda_mol)
-
-    return mm_mol
+    for file in top_file:
+       uni = mda.Universe(file, guess_bonds=guess_bonds)
+       mda_mol = mmic_mda.models.MdaMol(data=uni)
+       mm_mol = mmic_mda.components.MdaToMolComponent.compute(mda_mol)
 
 
 def test_mol_to_mda(guess_bonds):
-    mm_mol = mm.models.molecule.mm_mol.Mol.from_file(top_file)
-
-    return mmic_mda.components.MolToMdaComponent.compute(mm_mol)
+    for file in top_file:
+       mm_mol = mm.models.molecule.mm_mol.Mol.from_file(file)
+       mmic_mda.components.MolToMdaComponent.compute(mm_mol)
 
 
 def test_io_methods(guess_bonds):
-    mda_mol = mmic_mda.models.MdaMol.from_file(top_file, guess_bonds=guess_bonds)
-    assert isinstance(mda_mol.data, mda_mol.dtype)
+    for file in top_file:
+       mda_mol = mmic_mda.models.MdaMol.from_file(file, guess_bonds=guess_bonds)
+       assert isinstance(mda_mol.data, mda_mol.dtype)
 
-    mm_mol = mda_mol.to_schema()
-    assert isinstance(mm_mol, mm.models.molecule.Mol)
+       mm_mol = mda_mol.to_schema()
+       assert isinstance(mm_mol, mm.models.molecule.Mol)
