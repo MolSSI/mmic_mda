e8b67025841c00db22240d6fb0f6130cdab8288f
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-02-17 13:53:48 -0500

    fixed whitespace

--- mmic_mda/tests/test_mmic_mda_mol.py
+++ mmic_mda/tests/test_mmic_mda_mol.py
@@ -30,21 +30,21 @@ def test_mmic_mda_imported():
 
 def test_mda_to_mol(guess_bonds):
     for file in top_file:
-       uni = mda.Universe(file, guess_bonds=guess_bonds)
-       mda_mol = mmic_mda.models.MdaMol(data=uni)
-       mm_mol = mmic_mda.components.MdaToMolComponent.compute(mda_mol)
+        uni = mda.Universe(file, guess_bonds=guess_bonds)
+        mda_mol = mmic_mda.models.MdaMol(data=uni)
+        mm_mol = mmic_mda.components.MdaToMolComponent.compute(mda_mol)
 
 
 def test_mol_to_mda(guess_bonds):
     for file in top_file:
-       mm_mol = mm.models.molecule.mm_mol.Molecule.from_file(file)
-       mmic_mda.components.MolToMdaComponent.compute(mm_mol)
+        mm_mol = mm.models.molecule.mm_mol.Molecule.from_file(file)
+        mmic_mda.components.MolToMdaComponent.compute(mm_mol)
 
 
 def test_io_methods(guess_bonds):
     for file in top_file:
-       mda_mol = mmic_mda.models.MdaMol.from_file(file, guess_bonds=guess_bonds)
-       assert isinstance(mda_mol.data, mda_mol.dtype)
+        mda_mol = mmic_mda.models.MdaMol.from_file(file, guess_bonds=guess_bonds)
+        assert isinstance(mda_mol.data, mda_mol.dtype)
 
-       mm_mol = mda_mol.to_schema()
-       assert isinstance(mm_mol, mm.models.molecule.Molecule)
+        mm_mol = mda_mol.to_schema()
+        assert isinstance(mm_mol, mm.models.molecule.Molecule)
