971ec88782a6fd23b3fc4077059708d77322c43c
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-08-24 01:39:56 -0400

    complete compute method in TrajToMdaComponent

--- mmic_mda/components/traj_component.py
+++ mmic_mda/components/traj_component.py
@@ -9,7 +9,7 @@ from mmic_translator.models import (
     schema_input_default,
     schema_output_default,
 )
-
+import MDAnalysis as mda
 from ..mmic_mda import units
 
 __all__ = ["TrajToMdaComponent", "MdaToTrajComponent"]
@@ -39,13 +39,69 @@ class TrajToMdaComponent(TransComponent):
             inputs = self.input()(**inputs)
 
         mm_traj = inputs.schema_object
-        if hasattr(mm_traj, "mol"):
-            uni = mm_traj.mol.to_data("MDAnalysis")
+        nframes = inputs.schema_object.nframes
+        natoms = inputs.schema_object.natoms
+        ndim = inputs.schema_object.ndim  # does MDA support 2d or 1D trajs?
+        kwargs = inputs.keywords
+
+        if inputs.schema_object.geometry is not None:
+            coords = inputs.schema_object.geometry
+        if inputs.schema_object.velocities is not None:
+            velocities = inputs.schema_object.velocities
+            kwargs.setdefault("velocities", velocities)
+        if inputs.schema_object.forces is not None:
+            forces = inputs.schema_object.forces
+            kwargs.setdefault("forces", forces)
+
+        if mm_traj.top is not None:
+            top = mm_traj.top
+
+            if isinstance(top, (list, tuple)):
+                raise NotImplementedError("MDAnalysis supports only static topologies!")
+
+            data_object = mda.Universe.empty(
+                natoms,
+                # n_residues=nres,
+                # atom_resindex=resids,
+                # residue_segindex=segindices,
+                trajectory=True,
+                velocities="velocities" in kwargs,
+                forces="forces" in kwargs,
+            )
+
+            data_object.add_TopologyAttr("type", top.symbols)
+            data_object.add_TopologyAttr("mass", top.masses)
+            data_object.atoms.masses = convert(
+                data_object.atoms.masses, top.masses_units, units["mass"]
+            )
+
+            if top.atom_labels is not None:
+                data_object.add_TopologyAttr("name", top.atom_labels)
+            if top.connectivity is not None:
+                bonds = [
+                    (bond[0], bond[1]) for bond in top.connectivity
+                ]  # How to load bond order?
+                data_object.add_TopologyAttr("bonds", bonds)
+
+            for frame in range(nframes):
+                data_object.trajectory.ts.dt = inputs.schema_object.timestep
+                data_object.trajectory.ts.positions = coords[:, :, frame]
+                if "velocities" in kwargs:
+                    data_object.trajectory.ts.velocities = velocities[:, :, frame]
+                if "forces" in kwargs:
+                    data_object.trajectory.ts.forces = forces[:, :, frame]
+        else:
+            coords = coords.reshape(nframes, natoms, ndim)
+            kwargs.update(velocities=velocities.reshape(nframes, natoms, ndim))
+            kwargs.update(forces=forces.reshape(nframes, natoms, ndim))
+            kwargs.setdefault("dt", inputs.schema_object.timestep)
+
+            data_object = mda.Universe(coords, **kwargs)
 
         success = True
         return success, TransOutput(
             proc_input=inputs,
-            data_object=uni,
+            data_object=data_object,
             data_units=units,
             schema_version=inputs.schema_version,
             schema_name=schema_output_default,
