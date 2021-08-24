e1b76102f51b6dedb4e60b5869286988eebf05cf
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-08-24 01:20:40 -0400

    add test for mmschema to mda

--- mmic_mda/tests/test_mmic_mda_traj.py
+++ mmic_mda/tests/test_mmic_mda_traj.py
@@ -9,16 +9,14 @@ import sys
 import os
 import MDAnalysis as mda
 import mmelemental as mm
-
-
-data_dir = os.path.join("mmic_mda", "data")
-traj_file = os.path.join(data_dir, "trajectories", "traj.trr")
-top_file = os.path.join(data_dir, "molecules", "1dzl_fixed.pdb")
+import mm_data
 
 
 def pytest_generate_tests(metafunc):
     if "guess_bonds" in metafunc.fixturenames:
         metafunc.parametrize("guess_bonds", ["False", "True"])
+    if "md_traj_file" in metafunc.fixturenames:
+        metafunc.parametrize("md_traj_file", [mm_data.trajs["traj.trr"]])
 
 
 def test_mmic_mda_imported():
@@ -26,8 +24,8 @@ def test_mmic_mda_imported():
     assert "mmic_mda" in sys.modules
 
 
-def test_mda_to_traj(**kwargs):
-    traj = mda.Universe(traj_file)
+def test_mda_to_traj(md_traj_file, **kwargs):
+    traj = mda.Universe(md_traj_file)
     inputs = {
         "data_object": traj,
         "schema_version": 1,
@@ -39,7 +37,13 @@ def test_mda_to_traj(**kwargs):
     return mm_traj
 
 
-def test_mda_to_top_traj(guess_bonds, **kwargs):
+@pytest.mark.parametrize(
+    "top_file,traj_file",
+    [
+        (mm_data.mols["1dzl_fixed.gro"], mm_data.trajs["traj.trr"]),
+    ],
+)
+def test_mda_to_top_traj(guess_bonds, top_file, traj_file, **kwargs):
     traj = mda.Universe(top_file, traj_file, guess_bonds=guess_bonds)
     inputs = {
         "data_object": traj,
@@ -52,7 +56,34 @@ def test_mda_to_top_traj(guess_bonds, **kwargs):
     return mm_traj
 
 
-def test_io_methods(guess_bonds):
+@pytest.mark.parametrize(
+    "traj_file",
+    mm_data.trajs.values(),
+)
+def test_traj_to_mda(traj_file, **kwargs):
+    mm_traj = mm.models.Trajectory.from_file(traj_file, all_frames=True)
+
+    if mm_traj.ndim != 3:
+        return None
+
+    inputs = {
+        "schema_object": mm_traj,
+        "schema_version": 1,
+        "schema_name": "mmel_input",
+        "keywords": kwargs,
+    }
+    mda_traj = mmic_mda.components.TrajToMdaComponent.compute(inputs)
+
+    return mda_traj
+
+
+@pytest.mark.parametrize(
+    "top_file",
+    [
+        mm_data.mols["1dzl_fixed.gro"],
+    ],
+)
+def test_io_methods(guess_bonds, top_file):
     mda_traj = mmic_mda.models.MdaTraj.from_file(top_file, guess_bonds=guess_bonds)
     assert isinstance(mda_traj.data, mda_traj.dtype())
 
